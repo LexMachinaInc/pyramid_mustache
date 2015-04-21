011f293e51e308a81aea73ffec44fe7f7fa73e47
jdoranster <jdoranster@gmail.com>
2015-04-21 12:38:39 -0700

    add a test

---
+++ pyramid_mustache/tests/test_mustache.py
@@ -0,0 +1,32 @@
+
+from unittest import TestCase
+from pyramid_mustache import (
+    LexRenderer,
+)
+import pystache
+from pystache import Renderer
+
+our_template=u"""{{#Patent}}
+       <h4>
+         <a href="/patent/{{link_number}}">
+           <span class="label label-patent-big">{{link_number}}</span>
+         </a>
+         {{#title}}{{.}}{{/title}}
+       </h4>
+ {{/Patent}}"""
+
+
+class TestMustache(TestCase):
+
+    def test_mustache_default(self):
+        renderer = Renderer()
+        output = renderer.render(our_template, {'Patent':{'title':'foo-bar','link_number':[u'1234']}})
+        print 'default output %s', output
+        self.assertIn('[u', output)
+
+
+    def test_mustache_lex(self):
+        renderer = LexRenderer()
+        output = renderer.render(our_template, {'Patent':{'title':'foo-bar','link_number':[u'1234']}})
+        print 'output %s', output
+        self.assertNotIn('[u', output)
